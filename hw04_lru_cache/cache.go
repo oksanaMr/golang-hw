a24987fe8a8f3c7ff391a69048cdfa210e89ed86
omordasova <omordasova@diasoft.ru>
2026-01-21 09:53:26 +0300

    HW4 is complete

--- hw04_lru_cache/cache.go
+++ hw04_lru_cache/cache.go
@@ -9,11 +9,14 @@ type Cache interface {
 }
 
 type lruCache struct {
-	Cache // Remove me after realization.
-
 	capacity int
 	queue    List
 	items    map[Key]*ListItem
+}
+
+type cacheEntry struct {
+	key   Key
+	value interface{}
 }
 
 func NewCache(capacity int) Cache {
@@ -22,4 +25,48 @@ func NewCache(capacity int) Cache {
 		queue:    NewList(),
 		items:    make(map[Key]*ListItem, capacity),
 	}
+}
+
+func (lru *lruCache) Set(key Key, value interface{}) bool {
+	if item, ok := lru.items[key]; ok {
+		item.Value = cacheEntry{
+			key:   key,
+			value: value,
+		}
+		lru.queue.MoveToFront(item)
+		return ok
+	}
+
+	// Если достигли емкости, удаляем самый старый элемент
+	if lru.queue.Len() >= lru.capacity {
+		lastNode := lru.queue.Back()
+		if lastNode != nil {
+			delete(lru.items, lastNode.Value.(cacheEntry).key)
+			lru.queue.Remove(lastNode)
+		}
+	}
+
+	newEntry := cacheEntry{
+		key:   key,
+		value: value,
+	}
+	newItem := lru.queue.PushFront(newEntry)
+	lru.items[key] = newItem
+
+	return false
+}
+
+func (lru *lruCache) Get(key Key) (interface{}, bool) {
+	if item, ok := lru.items[key]; ok {
+		lru.queue.MoveToFront(item)
+
+		entry := item.Value.(cacheEntry)
+		return entry.value, true
+	}
+	return nil, false
+}
+
+func (lru *lruCache) Clear() {
+	lru.queue = NewList()
+	lru.items = make(map[Key]*ListItem, lru.capacity)
 }
