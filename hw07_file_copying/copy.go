6d4d906dc0a4c71ffe26b8b5bba7fe6f0a2d174e
omordasova <omordasova@diasoft.ru>
2026-02-11 09:28:48 +0300

    HW7 is complete

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,6 +2,10 @@ package main
 
 import (
 	"errors"
+	"io"
+	"os"
+
+	"github.com/cheggaaa/pb/v3"
 )
 
 var (
@@ -10,6 +14,52 @@ var (
 )
 
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
-	return nil
+	srcFile, err := os.Open(fromPath)
+	if err != nil {
+		return err
+	}
+	defer srcFile.Close()
+
+	fileInfo, err := srcFile.Stat()
+	if err != nil {
+		return err
+	}
+
+	fileSize := fileInfo.Size()
+	if fileSize == 0 {
+		return ErrUnsupportedFile
+	}
+
+	if offset > fileSize {
+		return ErrOffsetExceedsFileSize
+	}
+
+	_, err = srcFile.Seek(offset, io.SeekStart)
+	if err != nil {
+		return err
+	}
+
+	bytesToCopy := fileSize - offset
+	if limit > 0 && limit < bytesToCopy {
+		bytesToCopy = limit
+	}
+
+	dstFile, err := os.Create(toPath)
+	if err != nil {
+		return err
+	}
+	defer dstFile.Close()
+
+	bar := pb.Full.Start64(bytesToCopy)
+	defer bar.Finish()
+
+	var reader io.Reader = srcFile
+	if limit > 0 {
+		reader = io.LimitReader(srcFile, limit)
+	}
+
+	barReader := bar.NewProxyReader(reader)
+
+	_, err = io.Copy(dstFile, barReader)
+	return err
 }
--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,45 @@
 package main
 
-import "testing"
+import (
+	"bytes"
+	"os"
+	"testing"
+)
 
 func TestCopy(t *testing.T) {
-	// Place your code here.
+	t.Run("file", func(t *testing.T) {
+		srcFile := "testdata/input.txt"
+		dstFile := "out.txt"
+
+		expectedData, err := os.ReadFile(srcFile)
+		if err != nil {
+			t.Fatalf("Failed to read source file: %v", err)
+		}
+
+		defer os.Remove(dstFile)
+
+		err = Copy(srcFile, dstFile, 0, 0)
+		if err != nil {
+			t.Errorf("Unexpected error: %v", err)
+			return
+		}
+
+		// Проверяем, что файл создан
+		if _, err := os.Stat(dstFile); os.IsNotExist(err) {
+			t.Errorf("Destination file was not created")
+			return
+		}
+
+		// Читаем и проверяем содержимое
+		actualData, err := os.ReadFile(dstFile)
+		if err != nil {
+			t.Errorf("Failed to read destination file: %v", err)
+			return
+		}
+
+		if !bytes.Equal(actualData, expectedData) {
+			t.Errorf("Unexpected content\nexpected length: %d\nactual length:   %d",
+				len(expectedData), len(actualData))
+		}
+	})
 }
